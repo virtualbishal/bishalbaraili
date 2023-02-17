dc518134eb85743fd531563c8d9cd5f628cc21d0
virtualbishal <125664464+virtualbishal@users.noreply.github.com>
2023-02-17 18:44:53 +0545

    website upload

---
+++ script.js
@@ -0,0 +1,218 @@
+
+let left_btn=document.getElementsByClassName('bi-chevron-left')[0];
+let right_btn=document.getElementsByClassName('bi-chevron-right')[0];
+let cards=document.getElementsByClassName('cards')[0];
+let search_bx=document.getElementsByClassName('search_bx')[0];
+let search_input=document.getElementById('search_input');
+
+left_btn.addEventListener('click',()=>{
+    cards.scrollLeft-=140;
+
+});
+
+right_btn.addEventListener('click',()=>{
+    cards.scrollLeft+=140;
+
+});
+
+
+let json_url="movie.json";
+
+
+fetch(json_url).then(Response=>Response.json())
+.then((data) =>{
+    data.forEach((ele,i) => {
+        let{name, imdb, date, sposter, bposter, genre, url}=ele;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML=`
+        <img src="${sposter}" alt="${name}" class="poster">
+              <div class="rest_card">
+                <img src="${bposter}" alt="">
+                <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre},${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i>${imdb}</h3>
+                        </div>
+                </div>
+              </div>
+        `
+        cards.appendChild(card);
+        
+    
+    });
+
+    document.getElementById('title').innerText=data[0].name;
+    document.getElementById('gen').innerText=data[0].genre;
+    document.getElementById('date').innerText=data[0].date;
+    document.getElementById('rate').innerHTML=`<span>IMDB</span><i class="bi bi-star-fill"></i>${data[0].imdb}`;
+
+    // search_bx data load 
+    data.forEach(element => {
+        let{name, imdb, date, sposter, genre, url}=element;
+        let card=document.createElement('a'); 
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML=`
+
+      <img src="${sposter}" alt="">
+                    <div class="content">
+                        <h3>${name}</h3>
+                        <p>${genre},${date}<span> IMDB</span><i class="bi bi-star-fill"></i>${imdb}</p>
+                    </div>
+
+        `
+       search_bx.appendChild(card);
+      
+    });
+
+    // search filter
+    search_input.addEventListener('keyup',()=>{
+        let filter=search_input.value.toUpperCase();
+        let a =search_bx.getElementsByTagName('a');
+
+        for (let index = 0; index < a.length; index++) {
+            let b=a[index].getElementsByClassName('content')[0];
+            // console.log(a.textContent)
+            let TextValue=b.textContent||b.innerText;
+            if (TextValue.toUpperCase().indexOf(filter)>-1)
+             {
+                a[index].style.display="flex";
+                search_bx.style.visibility="visible";
+                search_bx.style.opacity=1;
+            }
+            else
+            {    
+                a[index].style.display="none";
+            }
+            if (search_input.value==0) {
+                search_bx.style.visibility="hidden";
+                search_bx.style.opacity=0;
+            }
+            
+        }
+    })
+
+    let video=document.getElementsByTagName('video')[0];
+    let play=document.getElementById('play');
+    play.addEventListener('click',()=>{
+
+   
+    if (video.paused)
+     {
+        video.play();
+        play.innerHTML=`Play<i class="bi bi-pause-fill"></i>`
+    }
+    else
+    {
+        video.pause();
+        play.innerHTML=`Watch<i class="bi bi-play-fill"></i>`
+    }
+   })
+
+   let series =document.getElementById('series');
+   let movie =document.getElementById('movie');
+   let naughty =document.getElementById('naughty');
+
+  
+
+
+   series.addEventListener('click',()=>{
+    cards.innerHTML=' ';
+
+    let series_array=data.filter(ele=>{
+     return ele.type=="series";
+    });
+    series_array.forEach((ele,i) => {
+        let{name, imdb, date, sposter, bposter, genre, url}=ele;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML=`
+        <img src="${sposter}" alt="${name}" class="poster">
+              <div class="rest_card">
+                <img src="${bposter}" alt="">
+                <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre},${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i>${imdb}</h3>
+                        </div>
+                </div>
+              </div>
+        `
+        cards.appendChild(card);
+        
+    
+    });
+
+    
+
+    movie.addEventListener('click',()=>{
+        cards.innerHTML=' ';
+    
+        let movie_array=data.filter(ele=>{
+         return ele.type=="movie";
+        });
+            movie_array.forEach((ele,i) => {
+            let{name, imdb, date, sposter, bposter, genre, url}=ele;
+            let card=document.createElement('a');
+            card.classList.add('card');
+            card.href=url;
+            card.innerHTML=`
+            <img src="${sposter}" alt="${name}" class="poster">
+                  <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                            <h4>${name}</h4>
+                            <div class="sub">
+                                <p>${genre},${date}</p>
+                                <h3><span>IMDB</span><i class="bi bi-star-fill"></i>${imdb}</h3>
+                            </div>
+                    </div>
+                  </div>
+            `
+            cards.appendChild(card);
+            
+        
+        });
+       })
+   })
+
+   naughty.addEventListener('click',()=>{
+    cards.innerHTML=' ';
+
+    let naughty_array=data.filter(ele=>{
+     return ele.type=="naughty";
+    });
+    naughty_array.forEach((ele,i) => {
+        let{name, imdb, date, sposter, bposter, genre, url}=ele;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML=`
+        <img src="${sposter}" alt="${name}" class="poster">
+              <div class="rest_card">
+                <img src="${bposter}" alt="">
+                <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre},${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i>${imdb}</h3>
+                        </div>
+                </div>
+              </div>
+        `
+        cards.appendChild(card);
+        
+    
+    });
+   })
+
+
+   
+
+   
+});
